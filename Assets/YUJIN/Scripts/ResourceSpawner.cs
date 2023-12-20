b5111c2924e0de6aa3b6500f4031b699bb6aa350
CEOCheon <1000yuhaa@gmail.com>
2023-12-21 02:21:54 +0900

    [Edit]
    ResourceSpawner Edit

--- Assets/YUJIN/Scripts/ResourceSpawner.cs
+++ Assets/YUJIN/Scripts/ResourceSpawner.cs
@@ -1,39 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class ResourceSpawner : MonoBehaviour
 {
     [SerializeField] private Resource[] resources;
     [SerializeField] private Transform[] SpawnSpots;
 
-    [SerializeField] private float spawnDuration; // 자원 리스폰 주기
-    private float timer; // 시간 계산을 위한 타이머
+    [SerializeField] private float spawnDuration; // 자원 생성 간격
+    private float timer; // 타이머
 
-    private void Awake()
-    {
-        Debug.Log("Resource생성?!?!?");
-    }
     void Start()
     {
-        Debug.Log("Resource생성?!?!?");
         timer = spawnDuration;
     }
+
     void Update()
     {
         timer -= Time.deltaTime;
 
-        if(timer <= 0)
-        {
-            Debug.Log("Resource생성?!?!?");
-            SpawneResource();
+        if (timer <= 0)
+        {  
+            SpawnResource();
             timer = spawnDuration;
         }
-
     }
-    public void SpawneResource()
+
+    private void SpawnResource()
     {
-        GameObject rsClone = Instantiate(resources[Random.Range(0, resources.Length)].gameObject, SpawnSpots[Random.Range(0, SpawnSpots.Length)]);
+        List<int> availableIndexes = new List<int>();
+        for (int i = 0; i < SpawnSpots.Length; i++)
+        {
+            if (SpawnSpots[i].childCount == 0) // 해당 스폰 위치에 자식이 없는 경우
+            {
+                availableIndexes.Add(i);
+            }
+        }
+
+        if (availableIndexes.Count > 0)
+        {
+            Debug.Log("리소스 생성");
+            int randomIndex = availableIndexes[Random.Range(0, availableIndexes.Count)];
+            Transform spawnSpot = SpawnSpots[randomIndex];
+
+            Resource resourceToSpawn = resources[Random.Range(0, resources.Length)];
+            Instantiate(resourceToSpawn.gameObject, spawnSpot.position, spawnSpot.rotation, spawnSpot);
+        }
+        else
+        {
+            Debug.Log("자리없뜸");
+        }
     }
 }
