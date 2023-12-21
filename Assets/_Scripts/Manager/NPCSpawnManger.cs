df0ef279a36eacdb21e210bb26455c55317e4040
Neunggwon <104968524+Neunggwon@users.noreply.github.com>
2023-12-21 11:55:36 +0900

    [FIX]
    NPC 수정

--- Assets/_Scripts/Manager/NPCSpawnManger.cs
+++ Assets/_Scripts/Manager/NPCSpawnManger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NPCSpawnManger : MonoBehaviour
 {
     public GameObject[] NPCPrefabs;
+    public Transform[] NPCSpawnPoints;
     private GameObject[] NPCCount;
     //public Transform spawnPoint;
     private int currentNPCCount = 0;
@@ -23,8 +24,9 @@ public class NPCSpawnManger : MonoBehaviour
             spawnDelay += Time.deltaTime;
             if (spawnDelay >= maxSpawnDelay)
             {
-                NPCSpawn();
-                Debug.Log("NPCSpawn");
+                //NPCSpawn();
+                NPCSpawnPoint();
+                //Debug.Log("NPCSpawn");
                 currentNPCCount++;
                 spawnDelay = 0;
             }
@@ -32,14 +34,27 @@ public class NPCSpawnManger : MonoBehaviour
         
     }
 
-    void NPCSpawn()
+    //void NPCSpawn()
+    //{
+    //    float randomX = transform.position.x + Random.Range(-spawnRadius, spawnRadius);
+    //    float randomZ = transform.position.z + Random.Range(-spawnRadius, spawnRadius);
+    //    Vector3 spawnPoint = new Vector3(randomX, 5, randomZ);
+    //    int randomNPC = Random.Range(0, NPCPrefabs.Length);
+
+    //    Instantiate(NPCPrefabs[randomNPC], spawnPoint, Quaternion.identity);
+    //    //Instantiate(NPCPrefabs[randomNPC], spawnPoint.position, spawnPoint.rotation);
+    //}
+    void NPCSpawnPoint()
     {
+        spawnRadius = 5;
         float randomX = transform.position.x + Random.Range(-spawnRadius, spawnRadius);
         float randomZ = transform.position.z + Random.Range(-spawnRadius, spawnRadius);
-        Vector3 spawnPoint = new Vector3(randomX, 10, randomZ);
+        Vector3 spawnPoint = new Vector3(randomX, 0, randomZ);
+
+        int randomSpawnPoint = Random.Range(0, NPCSpawnPoints.Length);
+
         int randomNPC = Random.Range(0, NPCPrefabs.Length);
 
-        Instantiate(NPCPrefabs[randomNPC], spawnPoint, Quaternion.identity);
-        //Instantiate(NPCPrefabs[randomNPC], spawnPoint.position, spawnPoint.rotation);
+        Instantiate(NPCPrefabs[randomNPC], NPCSpawnPoints[randomSpawnPoint].position + spawnPoint, Quaternion.identity);
     }
 }
